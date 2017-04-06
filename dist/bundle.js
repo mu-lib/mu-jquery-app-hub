7a04dffa1330919e921f307c48833c590d9d2fed
Mikael Karon <mikael@karon.se>
2017-04-06 22:08:33 +0200

    fix: remove broken CJS bundling

--- dist/bundle.js
+++ dist/bundle.js
@@ -62,9 +62,7 @@
 
   return function (modules, factory) {
     if (typeof define === "function" && define.amd) {
-      define(modules, factory);
-    } else if (typeof module === "object" && module.exports) {
-      module.exports = factory.apply(root, modules.map(require));
+      define(name, modules, factory);
     } else {
       root[name] = factory.apply(root, modules.map(function (m) {
         return root[m.replace(/^\./, prefix)] || m;
